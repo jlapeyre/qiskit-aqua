d81f61e58f8e7888e3bd34c45a20356f8c5d0108
John Lapeyre <jlapeyre@users.noreply.github.com>
2020-08-25 17:01:37 -0700

    Add improvements
    * Loosen restriction on "type" of phase estimation circuit
    * Prevent trying to add classical register twice
    * Rename identifiers

--- qiskit/aqua/algorithms/phase_estimators/phase_estimator.py
+++ qiskit/aqua/algorithms/phase_estimators/phase_estimator.py
@@ -1,7 +1,8 @@
 """The Quantum Phase Estimation Algorithm."""
 
 from typing import Dict, Optional, Union
-from qiskit.circuit.library import PhaseEstimation
+from qiskit.circuit import QuantumCircuit
+#from qiskit.circuit.library import PhaseEstimation
 from qiskit.circuit.classicalregister import ClassicalRegister
 from qiskit.providers import BaseBackend
 from qiskit.aqua import QuantumInstance
@@ -18,11 +19,29 @@ class PhaseEstimator(QuantumAlgorithm):
     """
 
     def __init__(self,
-                 phase_estimation: PhaseEstimation,
+                 pe_circuit: QuantumCircuit,
+                 num_evaluation_qubits,
+                 num_unitary_qubits,
+                 input_state = None,
                  quantum_instance: Optional[Union[QuantumInstance, BaseBackend]] = None):
 
-        self._phase_estimation = phase_estimation
+        self._pe_circuit = pe_circuit
+        self._num_evaluation_qubits = num_evaluation_qubits
+        self._num_unitary_qubits = num_unitary_qubits
+
         super().__init__(quantum_instance)
+        self._add_classical_register()
+
+    def _add_classical_register(self):
+        circ = self._pe_circuit
+        if not self._quantum_instance.is_statevector:
+            # Measure only the evaluation qubits.
+            regname = 'meas'
+            if not regname in [reg.name for reg in circ.cregs]:
+                creg = ClassicalRegister(self._num_evaluation_qubits, regname)
+                circ.add_register(creg)
+                circ.barrier()
+                circ.measure(range(self._num_evaluation_qubits), range(self._num_evaluation_qubits))
 
     # If we reversed the bit order of evaluation register circuit (including the iqft) then we would avoid
     # the binary calculations in the case of the statevector simulator.
@@ -31,13 +50,13 @@ class PhaseEstimator(QuantumAlgorithm):
         phase of 2pi. It is assumed that the input vector is an eigenvecter of the unitary so that
         the probability density is concentrated on one string (in the noiseless case).
         """
-        num_evaluation_qubits = self._phase_estimation.num_evaluation_qubits
+        num_evaluation_qubits = self._num_evaluation_qubits
         if self._quantum_instance.is_statevector:
             state_vec = result.get_statevector()
             # FIXME: Following digs into the implementation of PhaseEstimation circuit.
             evaluation_density_matrix = get_subsystem_density_matrix(
                 state_vec,
-                range(num_evaluation_qubits, num_evaluation_qubits + self._phase_estimation.num_unitary_qubits)
+                range(num_evaluation_qubits, num_evaluation_qubits + self._num_unitary_qubits)
             )
             diag = evaluation_density_matrix.diagonal()
             idx = np.argmax(abs(diag)) # np.argmax ignores complex part of number. But, we take abs anyway
@@ -55,15 +74,8 @@ class PhaseEstimator(QuantumAlgorithm):
         """Run the circuit and return the estimated phase as a number between 0.0 and 1.0, with 1.0 corresponding
         to a phase of 2pi.
         """
-        circ = self._phase_estimation
-        if not self._quantum_instance.is_statevector:
-            # Measure only the evaluation qubits.
-            creg = ClassicalRegister(circ.num_evaluation_qubits, 'meas')
-            circ.add_register(creg)
-            circ.barrier()
-            circ.measure(range(circ.num_evaluation_qubits), range(circ.num_evaluation_qubits))
 
-        result = self._quantum_instance.execute(circ)
+        result = self._quantum_instance.execute(self._pe_circuit)
         phase = self._compute_phase(result)
 
         return phase, result
