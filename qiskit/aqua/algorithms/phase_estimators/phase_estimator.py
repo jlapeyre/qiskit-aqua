f66c32656a8eb5fd4885beca66665993d41a4724
John Lapeyre <jlapeyre@users.noreply.github.com>
2020-08-31 20:09:33 -0700

    Checkpoint
    	modified:   qiskit/aqua/algorithms/phase_estimators/phase_estimator.py

--- qiskit/aqua/algorithms/phase_estimators/phase_estimator.py
+++ qiskit/aqua/algorithms/phase_estimators/phase_estimator.py
@@ -26,20 +26,20 @@ class PhaseEstimator(QuantumAlgorithm):
                  input_state_circuit = None,
                  quantum_instance: Optional[Union[QuantumInstance, BaseBackend]] = None):
 
-
         self._pe_circuit = pe_circuit
         self._num_evaluation_qubits = num_evaluation_qubits
         self._num_unitary_qubits = num_unitary_qubits
 
         if not input_state_circuit is None:
             self._pe_circuit = pe_circuit.compose(input_state_circuit,
-                                                  qubits=range(num_evaluation_qubits, num_evaluation_qubits + self._num_unitary_qubits),
+                                                  qubits=range(num_evaluation_qubits, num_evaluation_qubits + num_unitary_qubits),
                                                   front=True)
         else:
             self._pe_circuit = pe_circuit
 
         super().__init__(quantum_instance)
         self._add_classical_register()
+
 
     def _add_classical_register(self):
         """Explicitly add measurement instructions only if we are using a state vector simulator."""
@@ -54,16 +54,18 @@ class PhaseEstimator(QuantumAlgorithm):
                 circ.measure(range(self._num_evaluation_qubits), range(self._num_evaluation_qubits))
 
 
-    def compute_phases(self):
-        num_evaluation_qubits = self._num_evaluation_qubits
+    def _state_vector_phases(self):
+        state_vec = self._result.get_statevector()
+        evaluation_density_matrix = get_subsystem_density_matrix(
+            state_vec,
+            range(self._num_evaluation_qubits, self._num_evaluation_qubits + self._num_unitary_qubits)
+        )
+        phases = evaluation_density_matrix.diagonal().real # The diagonal is real
+        return phases
+
+    def _compute_phases(self):
         if self._quantum_instance.is_statevector:
-            state_vec = self._result.get_statevector()
-            evaluation_density_matrix = get_subsystem_density_matrix(
-                state_vec,
-                range(num_evaluation_qubits, num_evaluation_qubits + self._num_unitary_qubits)
-            )
-            diag = evaluation_density_matrix.diagonal().real # The diagonal is real
-            phases = diag
+            phases = self._state_vector_phases()
         else:
             # return counts with keys sorted numerically
             counts = self._result.get_counts()
@@ -71,47 +73,48 @@ class PhaseEstimator(QuantumAlgorithm):
             ck.sort() # Sorts in order integer encoded by binary string
             phases = {k : counts[k] for k in ck}
             phases = Counts(phases, memory_slots=counts.memory_slots, creg_sizes=counts.creg_sizes)
-#            phases = counts
 
-        return phases
+        self._phases = phases
 
     # If we reversed the bit order of evaluation register circuit (including the iqft) then we would avoid
     # the binary calculations in the case of the statevector simulator.
-    def _compute_phase(self):
+    def single_phase(self):
         """Return the estimated phase as a number between 0.0 and 1.0, with 1.0 corresponding to a
         phase of 2pi. It is assumed that the input vector is an eigenvecter of the unitary so that
         the probability density is concentrated on one string (in the noiseless case).
         """
-        num_evaluation_qubits = self._num_evaluation_qubits
         if self._quantum_instance.is_statevector:
-            state_vec = self._result.get_statevector()
-            # FIXME: Following digs into the implementation of PhaseEstimation circuit.
-            evaluation_density_matrix = get_subsystem_density_matrix(
-                state_vec,
-                range(num_evaluation_qubits, num_evaluation_qubits + self._num_unitary_qubits)
-            )
-            diag = evaluation_density_matrix.diagonal()
-            idx = np.argmax(abs(diag)) # np.argmax ignores complex part of number. But, we take abs anyway
-            binary_phase_string = np.binary_repr(idx, num_evaluation_qubits)
+            idx = np.argmax(abs(self._phases)) # np.argmax ignores complex part of number. But, we take abs anyway
+            binary_phase_string = np.binary_repr(idx, self._num_evaluation_qubits)
 
         else:
-            counts = self._result.get_counts()
-            binary_phase_string = max(counts, key=counts.get)
+            binary_phase_string = max(self._phases, key=self._phases.get)
 
         phase = index_to_phase(binary_phase_string)
 
         return phase
 
-    def _run(self) -> Dict:
+        # if self._quantum_instance.is_statevector:
+        #     phases = self._state_vector_phases()
+        #     idx = np.argmax(abs(phases)) # np.argmax ignores complex part of number. But, we take abs anyway
+        #     binary_phase_string = np.binary_repr(idx, self._num_evaluation_qubits)
+
+        # else:
+        #     counts = self._result.get_counts()
+        #     binary_phase_string = max(counts, key=counts.get)
+
+        # phase = index_to_phase(binary_phase_string)
+
+        # return phase
+
+    def _run(self):
         """Run the circuit and return the estimated phase as a number between 0.0 and 1.0, with 1.0 corresponding
         to a phase of 2pi.
         """
 
         result = self._quantum_instance.execute(self._pe_circuit)
         self._result = result
-        phase = self._compute_phase()
-
-        return phase, result
+        self._compute_phases()
 
 
 def index_to_phase(binary_string):
